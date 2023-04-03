d98705b858ae9b98bff88bf81021a860eb8a3950
antoninchafiol <antonin.chafiol@gmail.com>
2023-04-03 23:56:20 +0200

    Arranged folders/file tree

---
+++ functions/dataset.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def CustomDTS(Dataset):
+    def __init__(self, file, transform=None):
+        X = []
+        Y = []
+        for line in file.readlines():
+            Y.append([float(i) for i in line.split()][0])
+            X.append([float(i) for i in line.split()][1:])
+
+        X = np.array(X, dtype='float32')
+        Y = np.array(Y, dtype='int8')
+        self.X = X
+        self.Y = Y
+        self.transform=transform
+        
+    def __len__(self):
+        return len(self.Y)
+    def __getitem__(self, index):
+        return self.X[index],self.Y[index]
+    
