b923dffc1ab106c7af2c62692154d3db8802bf85
antoninchafiol <antonin.chafiol@gmail.com>
2023-04-04 00:56:48 +0200

    Update of filesystem

--- functions/dataset.py
+++ functions/dataset.py
@@ -1,21 +1,19 @@
 import numpy as np
+from torch.utils.data import Dataset
 
-def CustomDTS(Dataset):
+class CustomDTS(Dataset):
     def __init__(self, file, transform=None):
         X = []
         Y = []
-        for line in file.readlines():
+        for line in open(file,'r').readlines():
             Y.append([float(i) for i in line.split()][0])
             X.append([float(i) for i in line.split()][1:])
-
         X = np.array(X, dtype='float32')
         Y = np.array(Y, dtype='int8')
         self.X = X
         self.Y = Y
         self.transform=transform
-        
     def __len__(self):
         return len(self.Y)
     def __getitem__(self, index):
         return self.X[index],self.Y[index]
-    
